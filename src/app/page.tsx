da698039711d394d58cd15d085d93cc9abbbf5dd
sanjayshanmugam sk <sanjayshanmugamsk2108@gmail.com>
2023-09-05 16:49:26 +0530

    Setuped the width layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,34 @@
-
+import { BsSearch } from "react-icons/bs";
 import LeftSidebar from "./components/LeftSidebar";
 import MainComponent from "./components/MainComponent";
-
 
 const page = () => {
   return (
     <div className="w-full text-black flex justify-center items-center relative">
-      <div className="max-w-screen-xl w-full h-full flex relative">
+      <div className="max-w-[70vw] w-full h-full flex relative">
         {/* left sidebar for nav header */}
         <LeftSidebar />
-        <MainComponent/>
-        
-        <section>Right Section</section>
+        <MainComponent />
+
+        <section className="sticky top-4 w-[33%] flex flex-col h-screen items-stretch px-6 ">
+          <div>
+            <div className="relative w-full h-full ">
+              <label
+                htmlFor="searchBox"
+                className="absolute top-0 left-0 h-full flex items-center justify-center p-4 "
+              >
+                <BsSearch className="w-5 h-5 text-gray500" />
+              </label>
+              <input
+                id="searchBox"
+                type="text"
+                placeholder="Search "
+                className="outline-none focus:border-primary text-black focus:border bg-slate-100 w-full h-full rounded-full py-4  pl-14 pr-4"
+              />
+            </div>
+          </div>
+      
+        </section>
       </div>
     </div>
   );
